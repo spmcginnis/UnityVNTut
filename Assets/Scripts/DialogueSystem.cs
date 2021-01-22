68b2cc36c72fd44a020bcf0b8901257035518a26
spm <spmcginnis@users.noreply.github.com>
2021-01-22 17:40:10 -0600

    repo initialized. game at ep. 3 of the tutorial, adding dialogue

---
+++ Assets/Scripts/DialogueSystem.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class DialogueSystem : MonoBehaviour
+{
+    public static DialogueSystem instance;
+    public ELEMENTS elements;
+
+    public bool isSpeaking { get { return speaking != null; } }
+    [HideInInspector] public bool isWaitingForUserInput = false;
+
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// Say something and print it on the screen.
+    /// </summary>
+    public void Say(string speech, string speaker = "")
+    {
+        // First ensure no one is speaking
+        StopSpeaking();
+        speechText.text = targetSpeech;
+        // Start new coroutine for speech.
+        speaking = StartCoroutine(Speaking(speech, false, speaker));
+    }
+
+    public void SayAdd(string speech, string speaker = "")
+    {
+        StopSpeaking();
+        speechText.text = targetSpeech; // keeps text from jarbling if the key is pressed rapidly during an additive text segment.
+        speaking = StartCoroutine(Speaking(speech, true, speaker));
+    }
+
+    public void StopSpeaking()
+    {
+        // Make sure coroutine is not null before stopping it, otherwise we might get a null reference error.
+        if (isSpeaking)
+        {
+            StopCoroutine(speaking);
+            
+        }
+        speaking = null;
+    }
+
+    /// <summary>
+    /// Coroutine for speaking, as long as this is running, we know someone is speaking.
+    /// Responsible for showing the text and the speaker's name.
+    /// Also checks for current speech and for user input.
+    /// </summary>
+
+    string targetSpeech = "";
+    Coroutine speaking = null;
+    IEnumerator Speaking(string speech, bool additive = false, string speaker = "")
+    {
+        speechPanel.SetActive(true);
+        targetSpeech = speech;
+
+        if (!additive)
+        {
+            speechText.text = "";
+        } else {
+            targetSpeech = speechText.text + targetSpeech;
+        }
+
+        
+        speakerNameText.text = DetermineSpeaker(speaker);
+        isWaitingForUserInput = false;
+
+        // Output one character at a time
+        while(speechText.text != targetSpeech)
+        {
+            speechText.text += targetSpeech[speechText.text.Length];
+            yield return new WaitForEndOfFrame();
+        }
+
+        isWaitingForUserInput = true;
+        while(isWaitingForUserInput) // Tut omitted the curly braces here, but that seems off to me.  
+        {
+            yield return new WaitForEndOfFrame();
+        }
+        StopSpeaking();
+
+    }
+
+    string DetermineSpeaker(string s)
+    {
+        string retVal = speakerNameText.text; // default return is the current name
+        if (s != speakerNameText.text && s != "")
+        {
+            retVal = (s.ToLower().Contains("narrator")) ? "" : s;
+        }
+
+        return retVal;
+    }
+
+
+    [System.Serializable]
+    public class ELEMENTS
+    {
+        /// <summary>
+        /// The main panel containing all dialogue-related elements on the UI
+        /// </summary>
+        public GameObject speechPanel;
+        public Text speakerNameText;
+        public Text speechText;
+    }
+    public GameObject speechPanel { get { return elements.speechPanel; } }
+    public Text speakerNameText { get { return elements.speakerNameText; } }
+    public Text speechText { get { return elements.speechText; } }
+
+
+}
---
+++ Assets/Scripts/TestDialogue.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestDialogue : MonoBehaviour
+{
+    DialogueSystem dialogue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogue = DialogueSystem.instance;    
+    }
+
+    public string[] s = new string[]
+    {
+        "Hi, how are you? :Archer",
+        "It's lovely out today.",
+        "Don't you love this testing dialogue?",
+        "So rich and fun."
+    };
+
+    int index = 0;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (!dialogue.isSpeaking || dialogue.isWaitingForUserInput)
+            {
+                if (index >= s.Length)
+                {
+                    return;
+                }
+                Say(s[index]);
+                index++;
+
+            }
+        }
+    }
+
+    void Say(string s)
+    {
+        string[] parts = s.Split(':');
+        string speech = parts[0];
+        string speaker = (parts.Length >= 2) ? parts[1] : "";
+
+        dialogue.Say(speech, speaker);
+    }
+}
