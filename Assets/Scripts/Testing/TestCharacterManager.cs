44b3dd897b3a3136aa1d8da0eb52e95e27d6c48a
spm <spmcginnis@users.noreply.github.com>
2021-01-25 19:38:36 -0600

    added rem test sprites and script

--- Assets/Scripts/Testing/TestCharacterManager.cs
+++ Assets/Scripts/Testing/TestCharacterManager.cs
@@ -6,9 +6,8 @@ public class TestCharacterManager : MonoBehaviour
 {
     public Character Monk;
     public Character Muriel;
+    public Character Rem;
     
-
-
     public string[] speech;
     int i = 0;
     int j = 0;
@@ -28,8 +27,10 @@ public class TestCharacterManager : MonoBehaviour
     public float transitionSpeed = 2f;
     public bool smoothTransition = true;
 
-    // SECTION: Testing Muriel
+    // SECTION: Testing other characters
     public string[] murielIntro;
+    public string[] remIntro;
+
 
         // Start is called before the first frame update
     void Start()
@@ -39,7 +40,7 @@ public class TestCharacterManager : MonoBehaviour
         Monk.GetSprite(4);
 
         Muriel = CharacterManager.instance.GetCharacter("Muriel", enableNewCharacterOnStart: false);
-
+        Rem = CharacterManager.instance.GetCharacter("Rem", enableNewCharacterOnStart: false);
     }
 
     // Update is called once per frame
@@ -73,7 +74,59 @@ public class TestCharacterManager : MonoBehaviour
             }
         }
 
-        // Speech Handler
+        // Testing Rem
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            if (j < remIntro.Length)
+            {
+                if (!Rem.isEnabled)
+                {
+                    Rem.MoveTo(positionA, 100);
+                    Rem.isEnabled = true;
+                    Rem.MoveTo(positionB, moveSpeed, smooth);
+                }
+
+                
+                int facePose = 6;
+                
+                if (j == 0 || j==1) // smile
+                {
+                    facePose = 6;
+                }
+                
+                if (j == 2 || j==3) // smile blink
+                {
+                    facePose = 5;
+                }
+
+                if (j == 4) // frown blink
+                {
+                    facePose = 1;
+                }
+
+                if (j == 5) // frown talk
+                {
+                    facePose = 3;
+                }
+
+
+
+                Rem.StartFaceTransition(Rem.GetSprite("Rem", facePose), transitionSpeed, smoothTransition);
+
+                Rem.Say(remIntro[j]);
+                j++;
+            }
+                        else if (!endScene)
+            {
+                Rem.MoveTo(positionC, moveSpeed, smooth);
+                endScene = true;
+            } else
+            {
+                DialogueSystem.instance.Close(); //close and hide itself.
+            }
+        }
+
+        // Speech Handler // Testing Monk
         if (Input.GetKeyDown(KeyCode.Space))
         {
             if (i < speech.Length)
