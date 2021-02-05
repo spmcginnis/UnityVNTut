f1439354a8a942a100713966cedf632e392823fd
spm <spmcginnis@users.noreply.github.com>
2021-02-05 15:23:33 -0600

    empty classes for screenplay.  starting over at this point.

---
+++ Assets/Scripts/ScreenPlay.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenPlay
+{
+    // This should handle all of the screenplay elements, such as a list of speakers, as well as the text for a given section of screenplay.
+    // The text itself should be generated in another class using the properties and methods of this class.
+    // There should, perhaps, be a third class to manage the two.
+
+    // string castOfCharacters;
+    string speaker; // TODO refactor into a speaker object or a list of them.
+    string storyText;
+    string stageDirection; // A command line to be interpreted with a method.  
+    // string specialConditions; // Will need a way to handle things like whether a scene is accesible given the game state.
+
+    public ScreenPlay Line(string speaker = "", string storyText = "", string stageDirection = "")
+    {
+        this.speaker = speaker;
+        this.storyText = storyText;
+        this.stageDirection=stageDirection;
+        return this;
+    }
+}
---
+++ Assets/Scripts/Testing/TestScreenPlayLines.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScreenPlayLines : MonoBehaviour
+{
+    DialogueSystem dialogueSystem;
+    int lineIndex = 0;
+    List<ScreenPlay> testChapter;
+
+    void Start()
+    {
+        dialogueSystem = DialogueSystem.instance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
